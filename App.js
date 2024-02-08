b3f4962921590175148f215a3c26f47fa9af761b
LeoSeiti <Leonardoseiti1@gmail.com>
2024-02-08 11:56:46 -0300

    first mobile class

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App() {
+  const [num1, setNum1] = useState("0");
+  const [num2, setNum2] = useState("0");
+
+  function somar(){
+   let soma = parseInt(num1) + parseInt(num2);
+   console.log("Soma: ", soma);
+   alert("Soma: " + soma)
+  }
+
+  return(
+    <View style= {{position: "absolute",
+    top: 150,
+    right: 150,
+    left: 250,
+    backgroundColor: "lightgrey",
+    padding: 100}}>
+    
+      <Text>Digite o 1º numero </Text>
+      <TextInput style={{backgroundColor: "blue"}} value={num1} onChangeText={setNum1}/>
+      <Text>Digite o 2º numero </Text>
+      <TextInput style={{backgroundColor: "red"}} value={num2} onChangeText={setNum2}/>
+      <Button title='Somar' onPress={somar}/>
+      
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
